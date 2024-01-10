3297eca1b9d2932b9360a90bc116ed659f950f97
Jason <devop.jas@gmail.com>
2024-01-10 21:29:33 +0800

    learning dictionary

--- index.py
+++ index.py
@@ -1,4 +1,12 @@
 s = { 10,5,7};
 l = list(s);
 
-print(l[0]);
+# print(l[0]);
+r = set(l);
+print(r);
+print("DICTIONARY");
+d = {10:"unwired", 11:"learning"};
+print(d[10]);
+
+di = {1:"jason",2:"Evaristo"};
+print(di[2]);
