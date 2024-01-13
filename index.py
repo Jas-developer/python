9200ee83c8ebce7ff2b73f0304b6e489c5492399
Jason <devop.jas@gmail.com>
2024-01-14 05:55:02 +0800

    understanding escaoe

--- index.py
+++ index.py
@@ -1,9 +1,36 @@
-a = 500;
-b= 40;
-# print(a/b);
-# assignment operator
+# a = 500;
+# b= 40;
+# # print(a/b);
+# # assignment operator
 
-y = 20;
-v = y;
-z = v + y;
-print(z);
+# y = 20;
+# v = y;
+# z = v + y;
+# print(-z);
+
+# #relational operator;
+# a = 1115;
+# b=135;
+
+# c = a > b;
+# print(c);
+
+# c =  b or a;
+# print(c)
+
+
+my_name = "Jason ";
+print(my_name);
+my_string = "long  \
+    sample text \
+        more sample line\
+            more sample lines\
+        ";
+        
+another_strin = "another \n new line \n";
+print(another_strin);
+
+my_dir = "c:\Download\\nodejs";
+print(my_dir);
+
+new_string = "Joh said \"Hello\"to me";
