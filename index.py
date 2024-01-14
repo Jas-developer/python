9f3dc02b1d3cba3f189100fd27cb275475608e3a
Jason <devop.jas@gmail.com>
2024-01-14 09:19:02 +0800

    a

--- index.py
+++ index.py
@@ -1,36 +1,71 @@
-# a = 500;
-# b= 40;
-# # print(a/b);
-# # assignment operator
+# # a = 500;
+# # b= 40;
+# # # print(a/b);
+# # # assignment operator
 
-# y = 20;
-# v = y;
-# z = v + y;
-# print(-z);
+# # y = 20;
+# # v = y;
+# # z = v + y;
+# # print(-z);
 
-# #relational operator;
-# a = 1115;
-# b=135;
+# # #relational operator;
+# # a = 1115;
+# # b=135;
 
-# c = a > b;
-# print(c);
+# # c = a > b;
+# # print(c);
 
-# c =  b or a;
-# print(c)
+# # c =  b or a;
+# # print(c)
 
 
-my_name = "Jason ";
-print(my_name);
-my_string = "long  \
-    sample text \
-        more sample line\
-            more sample lines\
-        ";
+# my_name = "Jason ";
+# print(my_name);
+# my_string = "long  \
+#     sample text \
+#         more sample line\
+#             more sample lines\
+#         ";
         
-another_strin = "another \n new line \n";
-print(another_strin);
+# another_strin = "another \n new line \n";
+# print(another_strin);
 
-my_dir = "c:\Download\\nodejs";
-print(my_dir);
+# my_dir = "c:\Download\\nodejs";
+# print(my_dir);
 
-new_string = "Joh said \"Hello\"to me";
+# new_string = "Joh said \"Hello\"to me";
+
+# # input and output statements
+
+# # output
+# print("Hello world");
+# # print is a building function;
+# # arguments are passed to a function
+# # each function are different they have their own usability
+# print("input and output statements");
+
+# x = 5;
+# y= 5;
+# print("The resul of 5x5 is", x*y);
+# #using format method
+# print("Multiplication of {0} and {1} is {2}".format(2,3,2*3));
+# my_name = "Jason";
+# print("Hello my name is"+my_name+", welocome to the real world");
+# print("Hello %s, Welcome to hello world." %my_name);
+
+# #float
+# num = 123.4442;
+# print("The value is %.3f" % num);
+
+# # input statement
+# # sample of output statement is print();
+# my_name = input("Enter your name:");
+# print(my_name);
+ 
+# age = 20;
+# print(20)
+
+age = input("How old are you? ");
+int_age = int(age);
+print(int_age);
+print(type(int_age));
